2fc07c1260294acbb2cd749c10dbfa957fe50417
Selikem Asare-Brown <selikemba@gmail.com>
2023-02-16 17:15:19 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Main Entry
+ * main - Entry point
  *
- * return: Always (0) Success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return(0);
+	return (0);
 }
