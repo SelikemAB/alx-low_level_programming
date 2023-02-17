9be2d696e288ffa572d5a8367f23a16967df5ef8
Selikem Asare-Brown <selikemba@gmail.com>
2023-02-16 16:51:22 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* main - Main Entry point
+ *
+ * return: Always 0; (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programmimg is like building a multilingual puzzle")
+
+		return(0);
+}
