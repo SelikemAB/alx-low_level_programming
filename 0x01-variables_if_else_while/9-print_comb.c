15a6012421d6a7a7590446f165ccd6e629c073bb
Selikem Asare-Brown <selikemba@gmail.com>
2023-02-17 19:14:02 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - entry block
+ * void: no argument assigned
+ * Return: 0 -->  (Sucess)
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
