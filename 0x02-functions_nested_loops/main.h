daa23ce6b92936d1040acae4ece2941d39cf84e5
Selikem Asare-Brown <selikemba@gmail.com>
2023-02-21 20:35:46 -0800

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+ /**
+  * _putchar - writes/prints a character to stdout
+  *
+  * Function takes a char argument and returns the character.
+  *
+  * @c: The character
+  * Return: The character of interest
+  */
+int _putchar(char c);
+
+#endif
