3ec26f8adf3159c53443bad03a70a92202385f12
Selikem Asare-Brown <selikemba@gmail.com>
2023-02-23 17:48:55 -0800

    Task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,10 @@
  *
  * Checks if the charcater is uppercase or lowercase
  * */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
 #endif
