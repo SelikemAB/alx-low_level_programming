2d341a205e9c7f36362f49126708523ac30c48e0
Selikem Asare-Brown <selikemba@gmail.com>
2023-03-14 17:01:10 -0700

    Advance 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,51 +1,50 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * argstostr - concatenates all the arguments of a program.
- * @ac: argument count.
- * @av: argument vector.
- *
- * Return: pointer of an array of char
- */
+  *argstostr - concatenates all arguments of the program.
+  *@ac: argument count.
+  *@av: pointer to array of size ac.
+  *Return: NULL if ac == 0 or av == null, Pointer to new string.
+  *NULL on fail.
+  */
 char *argstostr(int ac, char **av)
 {
+	int i, j, k, size;
 	char *arg;
-	int c, i, j, size;
 
-	if (ac == 0)
+	size = 0;
+	k = 0;
+	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	for (c = i = 0; i < ac; i++)
+	i = 0;
+	while (i < ac)
 	{
-		if (av[i] == NULL)
-			return (NULL);
-
-		for (j = 0; av[i][j] != '\0'; j++)
-			c++;
-		c++;
-	}
-
-	arg = (char *) malloc((c + 1) * sizeof(char));
-
-	if (arg == NULL)
-	{
-		free(arg);
-		return (NULL);
-	}
-
-	for (i = j = size = 0; size < c; j++, size++)
-	{
-		if (av[i][j] == '\0')
+		j = 0;
+		while (av[i][j])
 		{
-			arg[size] = '\n';
-			i++;
 			size++;
-			j = 0;
+			j++;
 		}
-		if (size < c - 1)
-			arg[size] = av[i][j];
+		size++;
+		i++;
 	}
-	arg[size] = '\0';
-
+	arg = malloc((sizeof(char) * size) + 1);
+	if (arg == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			arg[k] = av[i][j];
+			j++;
+			k++;
+		}
+		arg[k] = '\n';
+		k++;
+		i++;
+	}
+	arg[k] = '\0';
 	return (arg);
+}
