b1c68f6efc4eeec1bacef14bd5465521c725e128
Selikem Asare-Brown <selikemba@gmail.com>
2023-03-20 15:59:38 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+/**
+ * init_dog - initializes a variable of type struct dog.
+ * @d: struct dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: no return.
+ */
+void init_dog(struct dog *p, char *name, float age, char *owner)
+{
+	if (p)
+	{
+		p->name = name;
+		p->age = age;
+		p->owner = owner;
+	}
+}
