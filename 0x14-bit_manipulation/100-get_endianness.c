14dc767d357e06f73c17981a27e93d86049fd73c
Selikem Asare-Brown <selikemba@gmail.com>
2023-04-10 15:00:21 -0700

    Task added

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks the endianness of the system.
+ * get_endianness - checks the endianness
  *
- * This function determines if the system is big endian or little endian by 
- * checking the value of the least significant byte of an unsigned int variable.
- *
- * Return: 0 if big endian, 1 if little endian.
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-    unsigned int x = 1;
-    char *c = (char *) &x; /* Casts the unsigned int pointer to a char pointer. */
+	unsigned int m;
+	char *c;
 
-    return ((int) *c);  /*Returns the value of the least significant byte. */
+	m = 1;
+	c = (char *) &m;
+
+	return ((int)*c);
 }
-
