9c710cb1e4bf6855bd528992a28cc0dd4a658a78
Selikem Asare-Brown <selikemba@gmail.com>
2023-04-10 14:20:03 -0700

    Task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,28 +2,28 @@
 #include <stdio.h>
 
 /**
-
-binary_to_uint - Convert a binary number to an unsigned int
-@b: The binary string to convert
-Return: The converted decimal number, or 0 if there is an unconvertible char
-*/
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
+ */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int total = 0, power = 1;
-int len = 0;
-if (b == NULL)
-return (0);
-/* Check that the input string is a valid binary number */
-for (; b[len]; len++)
-{
-if (b[len] != '0' && b[len] != '1')
-return (0);
-}
-/* Convert binary string to decimal */
-for (len--; len >= 0; len--, power *= 2)
-{
-if (b[len] == '1')
-total += power;
-}
-return (total);
-}
+	unsigned int value, expo;
+	int len;
+
+	if (b == NULL)
+		return (0);
+
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+
+	for (expo = 1, value = 0, len--; len >= 0; len--, expo *= 2)
+	{
+		if (b[len] == '1')
+			value += expo;
+	}
+
+	return (value);
